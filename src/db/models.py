dda568097777288e99bb0da2b8e9107f04c68af4
afsharino <mohammadafshari.fun.ac.ir@gmail.com>
2024-02-04 23:49:52 +0330

    add models.py to db

---
+++ src/db/models.py
@@ -0,0 +1,28 @@
+# Import Libraries
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+
+Base = declarative_base()
+
+
+class Book(Base):
+    __tablename__ = "book"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    rating = Column(Integer)
+    time_created = Column(DateTime(timezone=True), server_default=func.now())
+    time_updated = Column(DateTime(timezone=True), onupdate=func.now())
+    author_id = Column(Integer, ForeignKey("author.id"))
+
+    author = relationship("Author")
+
+
+class Author(Base):
+    __tablename__ = "author"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    time_created = Column(DateTime(timezone=True), server_default=func.now())
+    time_updated = Column(DateTime(timezone=True), onupdate=func.now())
